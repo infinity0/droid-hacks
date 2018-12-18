026c74a4a625b7779b1ca282bfa5b31eaca105d6
Ximin Luo <infinity0@pwned.gg>
2018-12-17 23:49:52 -0800

    Add restore-apk-data.sh

---
+++ listings/restore-apk-data.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Run this in /data/data/restore/ after you extract the backup.tar, and after
+# opening up the app for the first time (so expected files are created).
+set -e
+package="$1"
+
+chmod 771 "$package"
+chown -hR "$(stat -c %u:%g ../$package                    )" "$package"
+chcon -hR "$(ls -Zd        ../$package     | cut '-d ' -f1)" "$package"
+if [ -h ../$package/lib ]; then
+	ln -snf   "$(readlink      ../$package/lib                )" "$package"/lib
+	chown -h  "$(stat -c %u:%g ../$package/lib                )" "$package"/lib
+	chcon -h  "$(ls -Zd        ../$package/lib | cut '-d ' -f1)" "$package"/lib
+fi
+chmod 751 "$package"
+mv "../$package" "../_old_$package"
+mv "$package" ..
